ea9cfa59df03642fa82a404e7683e18f7278f24b
hhakim7 <40062252+hhakim7@users.noreply.github.com>
2019-01-15 15:04:59 +0100

    Update WhenTheUserEntersAnInitialGrid.java

--- gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/WhenTheUserEntersAnInitialGrid.java
+++ gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/WhenTheUserEntersAnInitialGrid.java
@@ -12,7 +12,6 @@ import net.thucydides.core.pages.Pages;
 import net.thucydides.junit.runners.ThucydidesRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
 
 @RunWith(ThucydidesRunner.class)
 @WithTag("Starting a new simulation")
